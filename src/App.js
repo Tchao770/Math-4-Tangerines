f69ef4be3e2baf70cc7f0306bd8cbeaeb309ab95
Tom <tchao770@gmail.com>
2021-02-23 23:24:29 -0800

    Initialization of project

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import OptionMenu from "./components/OptionMenu";
+
+function App() {
+    return (
+    <OptionMenu />
+    );
+}
+
+export default App;
---
+++ src/components/OptionMenu.js
@@ -0,0 +1,11 @@
+import { Button } from '@material-ui/core';
+
+function OptionMenu() {
+    return (
+        <div>
+            <Button>Math Options</Button>
+        </div>
+    );
+}
+
+export default OptionMenu;
