0b0e0441a1f183b27247db9b0752c71766420a58
Tom <tchao770@gmail.com>
2021-04-26 09:57:45 -0700

    Added test cases for problem generation

--- src/components/ProblemGen.js
+++ src/components/ProblemGen.js
@@ -1,9 +1,28 @@
+function Solve() {
+
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function getRandomOperator(operators) {
+    let index = Math.floor(Math.random() * operators.length);
+    return operators[index];
+}
+
 function ProblemGen({ min, max, count, operators }) {
     const problemset = [];
-    for(let i = 0; i < count; i++){
-        problemset.push(i);
+    for (let i = 0; i < count; i++) {
+        let problem =
+        {
+            a: getRandomInt(min, max),
+            b: getRandomInt(min, max),
+            operator: getRandomOperator(operators),
+        }
+        problemset.push(problem);
     }
     return problemset;
 }
 
-export { ProblemGen };
+export { ProblemGen, getRandomInt };
--- src/components/ProblemGen.test.js
+++ src/components/ProblemGen.test.js
@@ -1,5 +1,25 @@
-import { ProblemGen } from "./ProblemGen"
+import { ProblemGen, getRandomInt } from "./ProblemGen"
 
 test('Return size of array', () => {
-    expect(ProblemGen({ min: 0, max: 3, count: 5, operators: ['+', '-'] }).length).toBe(5);
-})
+    const genLength = ProblemGen({ min: 0, max: 3, count: 5, operators: ['+', '-'] }).length;
+    expect(genLength).toBe(5);
+});
+
+test('Return an array with 2 elements, both 1 + 1', () => {
+    const genProblem = ProblemGen({ min: 1, max: 1, count: 2, operators: ['+'] });
+    expect(genProblem).toEqual(
+        expect.arrayContaining([
+            expect.objectContaining({
+                a: 1,
+                b: 1,
+                operator: '+',
+            })
+        ])
+    );
+});
+
+test('Random number gen returns number in specified range', () => {
+    const randVal = getRandomInt(1, 5);
+    expect(randVal).toBeGreaterThanOrEqual(1);
+    expect(randVal).toBeLessThanOrEqual(5);
+});
