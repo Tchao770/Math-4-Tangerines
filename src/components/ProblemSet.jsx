219b30a2987b4c60ec85b8df57886bb72e9a7081
Tom <tchao770@gmail.com>
2021-04-26 17:25:52 -0700

    Placed problems in a grid

--- src/components/ProblemSet.jsx
+++ src/components/ProblemSet.jsx
@@ -1,16 +1,29 @@
+import { Container, Row, Col } from "react-bootstrap/";
 import { generateProblem } from "../logic/generateProblem.js";
-const problemList = generateProblem({ min: 1, max: 10, count: 100, operators: ['+', '-'] });
+
+const problemList = generateProblem({ min: 1, max: 10, count: 64, operators: ['+', '-'] });
+const newArr = [];
+const count = 64;
+const rows = Math.sqrt(count);
+while (problemList.length) newArr.push(problemList.splice(0, rows));
 
 function ProblemSet() {
     return (
-        <div>
-            {
-                problemList.map((problem, index) => {
-                    const { a, b, op, ans } = problem
-                    return <p key={index}>{a} {op} {b} = {ans}</p>
-                })
-            }
-        </div>
+        <Container>
+            {newArr.map((problemRow) => {
+                return (
+                    < Row >
+                        {problemRow.map((problem, index) => {
+                            const { a, b, op, ans } = problem;
+                            return (
+                                <Col>
+                                    <p key={index}>{a} {op} {b} = {ans}</p>
+                                </Col>)
+                        })
+                        }
+                    </Row>)
+            })}
+        </Container >
     );
 }
 
