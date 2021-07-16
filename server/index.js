9e752856cfc687eafb7ae4606960cb0a98a1421e
Tom <tchao770@gmail.com>
2021-07-16 15:16:11 -0700

    Backend structuring

--- server/index.js
+++ server/index.js
@@ -1,19 +1,38 @@
 const express = require('express');
-http = require('http');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-const hostname = 'localhost';
-const port = 8080;
+// Environment loading
+require('dotenv').config();
+
+// Express server creation
 const app = express();
+const port = process.env.PORT || 5000;
 
-app.use((req, res) => {
-    console.log(req.headers);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>This is a test server</h1></body></html>');
-});
+// ***************************** Middleware *****************************
+app.use(cors());
+app.use(express.json())
+// ***************************** Middleware *****************************
 
-const sample_server = http.createServer(app);
 
-sample_server.listen(port, hostname, ()=>{
-    console.log(`Server running at http:${hostname}:${port}`);
+
+// ****************************** MongoDB *******************************
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB connection esablished successfully");
+})
+// ****************************** MongoDB *******************************
+
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
+app.listen(port, () => {
+    console.log(`Server is running on port : ${port}`);
 });
